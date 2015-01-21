9a7b3aba7a4d4b3397256c1eb562c22d7139d62b
amjed <com_amjed@yahoo.com>
2015-01-21 11:59:11 +0100

    minor changes, add the lines to the boxplot , final development stage

--- database_connect_spectrumbridge.m
+++ database_connect_spectrumbridge.m
@@ -1,16 +1,44 @@
 function [response,delay,error]=database_connect_spectrumbridge(...
     DeviceType,Latitude,Longitude)
-%DATABASE_CONNECT_SPECTRUMBRIDGE Script used in querying SpectrumBridge WSDB [1].
+%DATABASE_CONNECT_SPECTRUMBRIDGE Script used in querying SpectrumBridge WSDB.
+%   Last update: 21 January 2015
+
+% Reference:
+%   P. Pawelczak et al. (2014), "Will Dynamic Spectrum Access Drain my
+%   Battery?," submitted for publication.
+
+%   Code development: Amjed Yousef Majid (amjadyousefmajid@student.tudelft.nl),
+%                     Przemyslaw Pawelczak (p.pawelczak@tudelft.nl)
+
+% Copyright (c) 2014, Embedded Software Group, Delft University of
+% Technology, The Netherlands. All rights reserved.
 %
-%   Reference: [1] Will Dynamic Spectrum Access Drain my Battery?
-
-%   Code development: 
-
-%   Last update: 29 June 2014
-
-%   This work is licensed under a Creative Commons Attribution 3.0 Unported
-%   License. Link to license: http://creativecommons.org/licenses/by/3.0/
-
+% Redistribution and use in source and binary forms, with or without
+% modification, are permitted provided that the following conditions
+% are met:
+%
+% 1. Redistributions of source code must retain the above copyright notice,
+% this list of conditions and the following disclaimer.
+%
+% 2. Redistributions in binary form must reproduce the above copyright
+% notice, this list of conditions and the following disclaimer in the
+% documentation and/or other materials provided with the distribution.
+%
+% 3. Neither the name of the copyright holder nor the names of its
+% contributors may be used to endorse or promote products derived from this
+% software without specific prior written permission.
+%
+% THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+% "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+% LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A
+% PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+% HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+% SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED
+% TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
+% PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+% LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+% NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+% SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 delay=[]; %Default delay value
 error=false; %Default error value
 
--- spectrumBridge_google_delay_comparison.m
+++ spectrumBridge_google_delay_comparison.m
@@ -1,4 +1,43 @@
+% SpectrumBridge google delay comparision compare the response time of
+% google WSDB and SpectrumBridge response. 
+%   Last update: 21 January 2015
 
+% Reference:
+%   P. Pawelczak et al. (2014), "Will Dynamic Spectrum Access Drain my
+%   Battery?," submitted for publication.
+
+%   Code development: Amjed Yousef Majid (amjadyousefmajid@student.tudelft.nl),
+%                     Przemyslaw Pawelczak (p.pawelczak@tudelft.nl)
+
+% Copyright (c) 2014, Embedded Software Group, Delft University of
+% Technology, The Netherlands. All rights reserved.
+%
+% Redistribution and use in source and binary forms, with or without
+% modification, are permitted provided that the following conditions
+% are met:
+%
+% 1. Redistributions of source code must retain the above copyright notice,
+% this list of conditions and the following disclaimer.
+%
+% 2. Redistributions in binary form must reproduce the above copyright
+% notice, this list of conditions and the following disclaimer in the
+% documentation and/or other materials provided with the distribution.
+%
+% 3. Neither the name of the copyright holder nor the names of its
+% contributors may be used to endorse or promote products derived from this
+% software without specific prior written permission.
+%
+% THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+% "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+% LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A
+% PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+% HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+% SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED
+% TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
+% PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+% LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+% NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+% SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 tic;
 clear;
 close all;
@@ -117,12 +156,14 @@ for xx=longitude_start:longitude_step:longitude_end
 end
 %%
 %Boxplot
-figure('Position',[440 378 560 420/3]);
+figure('Position',[440 378 560 620/3]);
 m = [delay_google' , delay_spectrumbridge'];
 boxplot(m , 'notch' , 'on', 'widths' , [box_width box_width], 'outliersize' ,outlierSize ,...
-    'positions' , [box1_pos box2_pos])
+    'positions' , [box1_pos box2_pos] , 'factorseparator',1)
 ylabel('Delay (sec)');
 set(gca , 'XTickLabel' , {'Google' , 'SpectrumBridge' });
+ax = gca;
+ax.YTick = [0:7];
 set(findobj(gca,'type','line'),'linew',lineWidth);
 set(findobj('type' , 'axes') , 'FontSize' , ftsz);
 set(findobj('type' , 'text'),'FontSize' , ftsz);
